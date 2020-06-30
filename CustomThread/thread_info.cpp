b1c417b38f6ed0c6b9e22f72a4a2640e428b5531
Stas Vilchikov <Donsgenii@gmail.com>
2020-06-30 20:52:49 +0300

    Add a description to thread_info.

--- CustomThread/thread_info.cpp
+++ CustomThread/thread_info.cpp
@@ -2,23 +2,29 @@
 
 #include <memory>
 
+thread_info::thread_info(thread_id_t new_id, size_t stack_size)
+	: stack_size(stack_size), id(new_id), eip(0)
+{
+	memset(gen_regs, NULL, GEN_REGS_MEMORY_SIZE);
+	//Alloc memory for stack used in thread
+	stack = new reg_t[stack_size];
+	//Set esp, ebp to the end of the stack
+	gen_regs[ESP_ID] =
+		gen_regs[EBP_ID] =
+		reinterpret_cast<reg_t>(stack + stack_size - 1);
+}
+
+//Constructor for the main thread. We don't init stack.
 thread_info::thread_info(thread_id_t new_id) :
 	thread_info(new_id, 0)
 {
 }
 
-thread_info::thread_info(thread_id_t new_id, size_t stack_size)
-	: stack_size(stack_size), id(new_id)
-{
-	stack = new reg_t[stack_size];
-
-	gen_regs[ESP_ID] =
-		gen_regs[EBP_ID] =
-		reinterpret_cast<std::uintptr_t>(stack + stack_size - 1);
-}
-
+//Constructor for a child thread. In this constructor, we set an address of a call-back
+//function(this function will free memory and delete thread_info form a list of threads) 
+//at the beginning of the stack and set eip to the beginning of the thread. 
 thread_info::thread_info(thread_id_t new_id, size_t stack_size, 
-	std::uintptr_t eip, std::uintptr_t ret) :
+	reg_t eip, reg_t ret) :
 	thread_info(new_id, stack_size)
 {
 	stack[stack_size - 1] = ret;
@@ -30,6 +36,7 @@ thread_info::~thread_info()
 	delete[] stack;
 }
 
+//We don't init stack only if its main thread(have a look at thread_info(thread_id_t)).
 bool thread_info::is_main() const
 {
 	return stack_size == 0;
--- CustomThread/thread_info.h
+++ CustomThread/thread_info.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include <cstdint>
-
 #include "reg_defines.h"
 
-using thread_id_t = uint32_t;
 
+//define a new type for thread_id, threads will use the id for dealing,
+//communication with other threads
+using thread_id_t = unsigned int;
 
+//in this structure, we save information about threads:
+//general purpose registers, eip, stack.....
 struct thread_info
 {
 	reg_t gen_regs[GEN_REGS_COUNT];
@@ -15,9 +17,15 @@ struct thread_info
 	size_t stack_size;
 	thread_id_t id;
 
+	//Constructor for the main thread.
 	thread_info(thread_id_t new_id);
+
+	//Auxiliary Constructor
 	thread_info(thread_id_t new_id, size_t stack_size);
-	thread_info(thread_id_t new_id, size_t stack_size, std::uintptr_t eip, std::uintptr_t ret);
+
+	//Constructor for a child thread.
+	thread_info(thread_id_t new_id, size_t stack_size, reg_t eip, reg_t ret);
+
 	~thread_info();
 
 	bool is_main() const;
