f27eee28d867f2a65d57685405c73309a633a912
Stas Vilchikov <Donsgenii@gmail.com>
2020-07-01 13:00:04 +0300

    The example has been updated to show how the "thread_fork()" works.

--- CustomThread/CustomThread.cpp
+++ CustomThread/CustomThread.cpp
@@ -17,7 +17,7 @@ void  fun2() {
         std::cout << "Fun2" << std::endl;
         thread_async_sleep(2000);
         if (i > 2) {
-            std::cout << "Fun 2 is going to suicide" << std::endl;
+            std::cout << "Fun2       is going to suicide" << std::endl;
             thread_kill(thread_id());
         }
         ++i;
@@ -25,7 +25,17 @@ void  fun2() {
 }
 
 void fun3() {
-    std::cout << "Fun3" << std::endl;
+    int code = thread_fork();
+    if (code) {
+        while (true)
+        {
+            std::cout << "Fun 3.1 " << std::endl;
+            thread_async_sleep(2000);
+        }
+    }
+    else {
+        std::cout << "Fun 3.0    EXIT" << std::endl;
+    }
 }
 
 int main()
@@ -33,11 +43,25 @@ int main()
     thread_init();
 
     thread_create(&fun2, STACK_SIZE);
-    thread_create(&fun1, STACK_SIZE);
+    thread_id_t thread_1 = thread_create(&fun1, STACK_SIZE);
     thread_create(&fun3, STACK_SIZE);
 
+    int i = 5;
     while (true) {
-        std::cout << "Main" << std::endl;
+        std::cout << "Main ";
+        if (thread_1) {
+            if (i != 0) {
+                std::cout << i;
+                --i;
+            }
+            else {
+                std::cout << "kill thread_1";
+                thread_kill(thread_1);
+                thread_1 = 0;
+            }
+        }
+        std::cout << std::endl;
+
         thread_async_sleep(3000);
     }
 
